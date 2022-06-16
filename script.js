d08410c599a14df9dbdfa744881e26c44f392822
monica kirabo <kirabom@duq.edu>
2022-06-16 09:39:57 -0700

    commit here

---
+++ script.js
@@ -0,0 +1,31 @@
+  let giphy_form=document.querySelector("form");
+const giphy_key="pcQ5NWNCpC7Ym16roiH4y5e2jDgcoWsp";
+const responsedata=document.getElementById("gif");
+const limit= 9;
+const rating="g";
+const giphy_url= 'http://api.giphy.com/v1/gifs/search?api_key=${giphy_key}&q=${responsedata}' ;
+console.log(responsedata);
+function getResults(){
+    
+var result=fetch(giphy_url)
+  .then(response => response.json())
+  .then(data => console.log(data));
+  console.log(result);
+}
+
+function displayResults(responsedata){
+
+    //const gifs=inputdata[giphy_url];
+responsedata.foreach((responsedata,i)){
+    document.getElementById("here").innerHTML+= <img src=" ${giphy_url}" alt="GIPHY image" />
+}
+}
+
+function handleFormSubmit(){
+var term=responsedata;
+}
+    
+   window.onload=function(){
+    
+   }
+
